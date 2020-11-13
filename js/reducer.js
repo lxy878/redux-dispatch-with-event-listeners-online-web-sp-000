aeb428f4560fb9e48cf5077b3a82d8691f3eb8c5
Xiaoyi Liu <lxy878@gmail.com>
2020-11-13 13:32:02 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+let state;
+
+function reducer(state={count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state
+  }
+}
+
+function dispatch(action){
+  state = reducer(state, action)
+  render()
+}
+
+function render(){
+  document.getElementById('container').textContent = state.count
+}
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', ()=>{
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+dispatch({type: '@@INIT'})
